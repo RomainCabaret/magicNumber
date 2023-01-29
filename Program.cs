a1ddb6d1277b894d821c6d95e3c2faa1cecb8a19
RomainCabaret <123275998+RomainCabaret@users.noreply.github.com>
2023-01-29 20:41:34 +0100

    Initial project

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace FirstProgramme
+{
+    class Program
+    {
+        static void StartGame(int heal)
+        {
+            Random rnd = new Random();
+            int magicNumber = rnd.Next(1, 10);
+
+            do
+            {
+                Console.WriteLine($"\nVies restantes : {heal}");
+                Console.Write($"Rentez un nombre entre 1 et 10 : ");
+
+                int userAnswerInt = 0;
+
+                while (userAnswerInt < 1 || userAnswerInt > 10)
+                {
+                    string userAnswerStr = Console.ReadLine();
+
+                    try
+                    {
+                        userAnswerInt = int.Parse(userAnswerStr);
+
+                        if (userAnswerInt <= 0)
+                        {
+                            throw new Exception("[Error] : incorrect the name must be between 1 and 10");
+                        }
+                    }
+                    catch
+                    {
+                        Console.Write($"Rentez un nombre entre 1 et 10 : ");
+                    }
+                }
+
+                if(userAnswerInt == magicNumber)
+                {
+                    Console.WriteLine($"\n -=== Bravo vous avez trouvé le nombre magique ===- \n");
+                    break;
+                }
+                else if(userAnswerInt < magicNumber)
+                {
+                    Console.WriteLine($" --Le nombre magique est plus grand-- ");
+                    heal--;
+                }
+                else
+                {
+                    Console.WriteLine($" --Le nombre magique est plus petit-- ");
+                    heal--;
+                }
+            }
+            while (heal != 0);
+
+            if (heal == 0)
+            {
+                Console.WriteLine($"\n -=== Vous avez perdu, le nombre magique était {magicNumber} ===- \n");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            StartGame(4);
+        }
+    }
+}
